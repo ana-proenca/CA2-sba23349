1744a6919244434e7e40ecb315d92f663dc64d13
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2024-03-03 14:06:59 -0300

    ArrayList EmployeeTest Class

--- src/ca2/sba23349/EmployeeTest.java
+++ src/ca2/sba23349/EmployeeTest.java
@@ -4,6 +4,8 @@
  */
 package ca2.sba23349;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author anaclaudiaproenca
@@ -17,5 +19,13 @@ public class EmployeeTest {
         Employee employee1 = new Employee("Joe Bloggs", "jb@gmail.com");
         Employee employee2 = new Employee("Ann Banana", "ab@gmail.com");
         Employee employee3 = new Employee("Tom Thumb", "tt@gmail.com");
+        
+        ArrayList<Employee> projectGroup = new ArrayList<>();
+        projectGroup.add(employee1);
+        projectGroup.add(employee2);
+        projectGroup.add(employee3);
+        
+        System.out.println(Employee.nextEmpNum);
+        
     }
 }
