b5451a6ab0a98c1dbde5822f71cd9582eba5d9ff
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2024-03-03 15:32:35 -0300

    Add the console menu and username and inputs.

--- src/ca2/sba23349/EmployeeTest.java
+++ src/ca2/sba23349/EmployeeTest.java
@@ -5,10 +5,11 @@
 package ca2.sba23349;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
- * @author anaclaudiaproenca
+ * @author anaclaudiaproenca https://github.com/ana-proenca/CA2-sba23349
  */
 public class EmployeeTest {
 
@@ -16,26 +17,57 @@ public class EmployeeTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Employee employee1 = new Employee("Joe Bloggs", "jb@gmail.com");
+        //Create the manager object that pass the username, password, name and email that are from the employee class.
+        Manager employee1 = new Manager("Gnomeo", "smurf", "Joe Bloggs", "jb@gmail.com");
+
         Employee employee2 = new Employee("Ann Banana", "ab@gmail.com");
         Employee employee3 = new Employee("Tom Thumb", "tt@gmail.com");
-        
+
         ArrayList<Employee> projectGroup = new ArrayList<>();
-        projectGroup.add(employee1);
+        projectGroup.add(employee1); //Add the manager as list of employee
         projectGroup.add(employee2);
         projectGroup.add(employee3);
-        
+
         System.out.println(Employee.nextEmpNum);
-        
-        int m = 2;
-        
-        for(Employee search : projectGroup) {
-            int empNumber = search.getEmployeeNumber();
-            
-            if(empNumber > m){
-                System.out.println(search.getName());
+
+        int m = 1;
+
+//            for (Employee search : projectGroup) {
+//                int empNumber = search.getEmployeeNumber();
+//
+//                if (empNumber > m) {
+//                    System.out.println(search.getName());
+//                }
+//            }
+        Company company = new Company();
+
+        //Load the current staffs to the company class
+        company.addNewStaff(employee1);
+        company.addNewStaff(employee2);
+        company.addNewStaff(employee3);
+
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Inform your username");
+            String username = sc.next();
+
+            System.out.println("Inform your password");
+            String password = sc.next();
+
+            //Check whether the username is valid or not
+            if (!employee1.getUsername().equals(username)) {
+                System.out.println("Invalid username");
             }
+
+            //Check whether the password is valid or not
+            if (!employee1.getPassword().equals(password)) {
+                System.out.println("Invalid password");
+            }
+            
+            company.listEmployee(m);
+
+        } catch (Exception e) {
         }
-        
     }
 }
